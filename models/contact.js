6a1b969c6c178452e244cbfac10223085c71cdfc
iankaFil <108305015+iankaFil@users.noreply.github.com>
2023-05-07 23:23:38 +0600

    ready

--- models/contact.js
+++ models/contact.js
@@ -1,23 +1,30 @@
+const { Schema, model } = require('mongoose');
 
-const mongoose = require('mongoose');
+const contactSchema = new Schema(
+    {
+        name: {
+            type: String,
+            required: [true, 'Set name for contact'],
+        },
+        email: {
+            type: String,
+        },
+        phone: {
+            type: String,
+        },
+        favorite: {
+            type: Boolean,
+            default: false,
+        },
+        owner: {
+            type: Schema.Types.ObjectId,
+            ref: 'user',
+            required: true,
+        },
+    },
+    { versionKey: false }
+);
 
-const contactSchema = new mongoose.Schema({
-    name: {
-        type: String,
-        required: [true, 'Set name for contact'],
-    },
-    email: {
-        type: String,
-    },
-    phone: {
-        type: String,
-    },
-    favorite: {
-        type: Boolean,
-        default: false,
-    },
-});
-
-const Contact = mongoose.model('Contact', contactSchema, 'contacts');
+const Contact = model('contact', contactSchema);
 
 module.exports = Contact;
--- routes/api/contacts.js
+++ routes/api/contacts.js
@@ -1,84 +1,43 @@
 const express = require('express');
-const contacts = require('../../models/contacts');
 
-const { createContactSchema, updateContactSchema } = require('../../utils/validation/contactsValidationSchemas');
+const { contacts: ctrl } = require('../../controllers');
+const validation = require('../../middlewares/validation');
+const {
+    addContactSchema,
+    updateContactSchema,
+    updateStatusContactSchema,
+} = require('../../schemas/contacts');
+const isValidId = require('../../middlewares/isValidId');
+const authenticate = require('../../middlewares/authenticate');
 
-const router = express.Router();
+const router = express();
 
-router.get('/', async (req, res, next) => {
-    const result = await contacts.getContacts();
-    res.json(result);
-});
+router.get('/', authenticate, ctrl.listContacts);
 
-router.get('/:contactId', async (req, res, next) => {
-    try {
-        const contact = await contacts.getContactById(req.params.contactId);
-        res.json(contact);
-    } catch (err) {
-        res.status(404).json({ message: err.message });
-    }
-});
+router.get('/:contactId', authenticate, isValidId, ctrl.getContactById);
 
-router.post('/', async (req, res, next) => {
-    const { name, email, phone } = req.body;
-    const { error } = createContactSchema.validate(req.body);
-    if (error) {
-        return res.status(400).json({ message: error.details[0].message });
-    } else {
-        const newContact = {
-            name,
-            email,
-            phone,
-        };
+router.post(
+    '/',
+    authenticate,
+    validation.addValid(addContactSchema),
+    ctrl.addContact
+);
 
-        const contact = await contacts.addContact(newContact);
-        if (contact) {
-            res.status(201).json(contact);
-        }
-    }
-});
+router.delete('/:contactId', authenticate, isValidId, ctrl.removeContact);
 
-router.delete('/:contactId', async (req, res, next) => {
-    try {
-        const contact = await contacts.removeContact(req.params.contactId);
-        if (contact) {
-            res.json({ message: 'contact deleted' });
-        }
-    } catch (err) {
-        res.status(404).json({ message: err.message });
-    }
-});
+router.put(
+    '/:contactId',
+    authenticate,
+    isValidId,
+    validation.updateValid(updateContactSchema),
+    ctrl.updateContact
+);
 
-router.put('/:contactId', async (req, res, next) => {
-    const { error } = updateContactSchema.validate(req.body);
-    if (error) {
-        return res.status(400).json({ message: error.details[0].message });
-    } else {
-        try {
-            const contact = await contacts.updateContact(req.params.contactId, req.body);
-            if (contact) {
-                res.json(contact);
-            }
-        } catch (err) {
-            res.status(404).json({ message: err.message });
-        }
-    }
-});
-
-router.patch('/:contactId/favorite', async (req, res) => {
-    const { contactId } = req.params;
-    const { favorite } = req.body;
-
-    if (!favorite) {
-        return res.status(400).json({ message: 'missing field favorite' });
-    }
-
-    try {
-        const updatedContact = await contacts.updateStatusContact(contactId, { favorite });
-        res.json(updatedContact);
-    } catch (error) {
-        res.status(404).json({ message: 'Not found' });
-    }
-});
+router.patch(
+    '/:contactId/favorite',
+    authenticate,
+    validation.updateFavValid(updateStatusContactSchema),
+    ctrl.updateStatusContact
+);
 
 module.exports = router;
